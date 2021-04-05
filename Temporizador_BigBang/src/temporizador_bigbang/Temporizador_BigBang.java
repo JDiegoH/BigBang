aa97153b0558d7ce4c1fd2e981506c3a9b227fe2
JDiegoH <81979990+JDiegoH@users.noreply.github.com>
2021-04-05 12:21:25 -0500

    Add files via upload
    Desarrollo interfaz gráfica, parte superior

---
+++ Temporizador_BigBang/src/temporizador_bigbang/Temporizador_BigBang.java
@@ -0,0 +1,12 @@
+package temporizador_bigbang;
+
+import temporizador_bigbang.Ventana;
+
+public class Temporizador_BigBang {
+
+    public static void main(String[] args) {
+        
+        Ventana ventana = new Ventana();
+    }
+    
+}
